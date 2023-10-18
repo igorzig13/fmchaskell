097cad2c5d3bc8e0a8df8894f6bc29e075c990d9
Igor Marques <igorzig13@gmail.com>
2023-10-17 21:59:33 -0300

    Adds Nat.hs

---
+++ Nat.hs
@@ -0,0 +1,17 @@
+module Nat where
+import Prelude hiding ((+), (*), (^))
+
+data Nat = O | S Nat
+    deriving ( Eq , Show )
+
+(+) :: Nat -> Nat -> Nat
+n + O = n
+n + S m = S (n+m)
+
+(*) :: Nat -> Nat -> Nat
+n * O = O
+n * S m = (n * m) + n
+
+(^) :: Nat -> Nat -> Nat
+n ^ O = S O
+n ^ (S m) = (n ^ m) * n
